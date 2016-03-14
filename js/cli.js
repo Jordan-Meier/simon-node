0472792fe2c8c51e56a21cd0ba16300a953bf427
Jordan Meier and Yvonna Contreras <jbirdmeier@gmail.com>
2016-03-14 08:54:49 -0700

    Create cli

--- js/cli.js
+++ js/cli.js
@@ -1,10 +1,22 @@
 var pingPong = require('./ping-pong.js').pingPong;
-var prompt = require('prompt');
-prompt.start();
 
-prompt.get('goal', function(err, result) {
-  var result = pingPong(result.goal);
-  result.forEach(function(element) {
-    console.log(element);
-  });
+
+console.log('PingPong up to 100');
+var result = pingPong(100);
+result.forEach(function(element){
+  console.log(element);
 });
+
+
+
+
+
+// var prompt = require('prompt');
+// prompt.start();
+//
+// prompt.get('goal', function(err, result) {
+//   var result = pingPong(result.goal);
+//   result.forEach(function(element) {
+//     console.log(element);
+//   });
+// });
