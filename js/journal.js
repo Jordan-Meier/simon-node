0b10e72d28ed6587185603aee704a313a2cc3d5c
Jordan Meier and Yvonna Contreras <yvonnap.contreras@gmail.com>
2016-03-14 13:21:03 -0700

    add initial project files and folders

--- js/journal.js
+++
@@ -1,9 +0,0 @@
-exports.Entry = function(title, body) {
-  this.title = title;
-  this.body = body;
-};
-
-exports.Entry.prototype.numberOfWords = function () {
-  var words = this.body.split(" ");
-  return words.length;
-};
